67ac08d138b249bf354786ea064ae3dba6b43ae8
Victor <vhgm294@gmail.com>
2024-09-05 20:08:50 -0500

    se distribuye el template home, y se agrega la app categorias y se crea su modelo

--- Categorias/models.py
+++ Categorias/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Categorias(models.Model):
+    nombre_categoria = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(max_length=255, blank=True)
+    imagen = models.ImageField(upload_to='fotos/categorias', blank=True)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+
+
+    def __str__(self):
+        return self.nombre_categoria
