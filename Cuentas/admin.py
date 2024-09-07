0bf9a4b5b423222bf3c9362cc122870707885f72
Victor <vhgm294@gmail.com>
2024-09-07 17:42:07 -0500

    se crea una funcion que muestra los datos del usuario desde admin, se impide la modificacion de la contraseña, y configura el apartado para permitir subir archivos multimedia

--- Cuentas/admin.py
+++ Cuentas/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Cuenta
+
 
 # Register your models here.
 
-admin.site.register(Cuenta)
+class CuentaAdmin(UserAdmin):
+    list_display = ('correo', 'nombre', 'apellido', 'username','ultima_sesion', 'fecha_ingreso', 'activo')
+    list_display_links = ('correo', 'nombre', 'apellido')
+    readonly_fields = ('ultima_sesion', 'fecha_ingreso')
+    ordering = ('-fecha_ingreso',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Cuenta, CuentaAdmin)
