27a043c98655c4020ba0366e54e174d1804b3a02
Jared D'Angleo Russo <jareddangelorusso@gmail.com>
2024-09-08 22:49:20 +0200

    Se crea la vista Tienda, se crea su ruta(Url). Se despliegan los productos desde la base de datos, se crea una funcion Count para contar los productos desplegados en la tienda.

--- Tienda/views.py
+++ Tienda/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
+
+def Tienda(request):
+    productos = Producto.objects.all().filter(esta_disponible=True)
+    producto_contador = productos.count()
+
+    context = {
+        'productos': productos,
+        'producto_contador': producto_contador,
+    } 
+    return render(request, 'Tienda/Tienda.html', context)
