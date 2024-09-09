8084f7e2112f2e92757d684c9b88d0ca97fc264d
Victor <vhgm294@gmail.com>
2024-09-09 14:12:12 -0500

    Se configura la ruta y vista para el carrito y se crea su modelo

--- Carrito/admin.py
+++ Carrito/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import carrito, carritoItem
 
 # Register your models here.
+
+admin.site.register(carrito)
+admin.site.register(carritoItem)
--- Carrito/models.py
+++ Carrito/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from Tienda.models import Producto
 
 # Create your models here.
+
+class carrito(models.Model):
+    id_carrito = models.CharField(max_length=250, blank=True)
+    fecha_agregada = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id_carrito
+
+
+class carritoItem(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    carrito = models.ForeignKey(carrito, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.producto
