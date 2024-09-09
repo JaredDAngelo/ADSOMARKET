5c8883656f94e26e0fcd572aadc0528af738abca
Jared D'Angleo Russo <jareddangelorusso@gmail.com>
2024-09-09 07:28:44 +0200

    se configuran las rutas para ver cada producto individualmente, deste la vista de home, tambien la de tienda, y se crean algunas rutas adicionales (URLS) para otros botones de las vistas.

--- Tienda/models.py
+++ Tienda/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from Categorias.models import Categorias
+from django.urls import reverse
 
 # Create your models here.
 class Producto(models.Model):
@@ -13,6 +14,9 @@ class Producto(models.Model):
     categoria            = models.ForeignKey(Categorias, on_delete=models.CASCADE)
     crear_fecha          = models.DateTimeField(auto_now_add=True)
     modificar_fecha      = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('producto_detalles', args=[self.categoria.slug, self.slug])
 
     def __str__(self):
         return self.producto_nombre
