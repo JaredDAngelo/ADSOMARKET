20d4a505c6d0d65e53025c9efc5bd6b7d60878b8
Jared D'Angleo Russo <jareddangelorusso@gmail.com>
2024-09-11 01:06:55 +0200

    Se crean y corrigen varias direcciones para algunos botones, se crea una funcion que verifica si un producto ya esta en el carrito, y en la vista de el producto individual se crean 2 botones nuevos(uno que indica que ya esta agregado, y otro para seguir comprando que te redirige a la pagina principal) si el producto ya esta en el carrito.

--- Tienda/views.py
+++ Tienda/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Producto
 from Categorias.models import Categorias
+from Carrito.models import carritoItem
+
+from Carrito.views import _id_carrito
+
 
 # Create your views here.
 
@@ -25,10 +29,17 @@ def Tienda(request, categoria_slug=None):
 def producto_detalles(request, categoria_slug, producto_slug):
     try:
         producto_individual= Producto.objects.get(categoria__slug=categoria_slug , slug=producto_slug)
+        en_carrito = carritoItem.objects.filter(carrito__id_carrito=_id_carrito(request), producto=producto_individual).exists()
+
+
+
     except Exception as e:
         raise e 
     
     context ={
         'producto_individual': producto_individual,
+        'en_carrito'         : en_carrito,
     }
     return render(request, 'Tienda/producto_detalles.html', context)
+
+###
