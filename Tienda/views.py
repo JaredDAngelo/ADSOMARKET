511b418651a533469dfdd4f142e539a4a47a8eb2
Victor <vhgm294@gmail.com>
2024-09-11 10:59:25 -0500

    Se utilizó un paginador para implementar la funcionalidad de los botones de 'anterior' y 'siguiente', así como los botones de números para navegar entre páginas. Cada botón genera su respectiva ruta de página para mostrar el contenido correspondiente.

--- Tienda/views.py
+++ Tienda/views.py
@@ -4,6 +4,7 @@ from Categorias.models import Categorias
 from Carrito.models import carritoItem
 
 from Carrito.views import _id_carrito
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
@@ -15,13 +16,19 @@ def Tienda(request, categoria_slug=None):
     if categoria_slug != None:
         categorias = get_object_or_404(Categorias, slug=categoria_slug)
         productos = Producto.objects.filter(categoria=categorias, esta_disponible= True)
+        paginator = Paginator(productos, 6)
+        page = request.GET.get("page")
+        paged_producto = paginator.get_page(page)
         producto_contador = productos.count()
     else:
         productos = Producto.objects.all().filter(esta_disponible=True)
+        paginator = Paginator(productos, 3)
+        page = request.GET.get("page")
+        paged_producto = paginator.get_page(page)
         producto_contador = productos.count()
 
     context = {
-        'productos': productos,
+        'productos': paged_producto,
         'producto_contador': producto_contador,
     } 
     return render(request, 'Tienda/Tienda.html', context)
