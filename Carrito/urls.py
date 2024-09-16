167a3885b97c103f471bcb9a29049ec1e823f58d
Jared D'Angleo Russo <jareddangelorusso@gmail.com>
2024-09-16 07:01:22 +0200

    se agrega la vista de Pagos, se crea un formulario nuevo y sus respectivas rutas

--- Carrito/urls.py
+++ Carrito/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('agregar_carrito/<int:id_producto>/', views.agregar_carrito, name='agregar_carrito'),
     path('quitar_carrito/<int:id_producto>/', views.quitar_carrito, name='quitar_carrito'),
     path('quitar_carrito_item/<int:id_producto>/', views.quitar_carrito_item, name='quitar_carrito_item'),
+
+    path('pagos/', views.pagar, name='pagos')
 ]
